52387f2e98ebe463d4a07e5bd43793572953e49e
abirami <81287215+abi-sender@users.noreply.github.com>
2026-07-09 16:51:01 -0500

    Initial full stack todo app

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
-// MongoDB Connection
-const connectDB=async()=>
-{
-    try
-    {
-    await mongoose.connect("mongodb://127.0.0.1:27017/tododb");
-    console.log("MongoDB Connected")
-    }
-  catch(err)
-  {
-console.log(err);
-  } 
-}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("MongoDB Connection Error:", err.message);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./router/todoroutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// DB connect
 connectDB();
 
-app.use("/",router);
+app.use("/", router);
 
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server Running on Port ${process.env.PORT || 5000}`);
 });
